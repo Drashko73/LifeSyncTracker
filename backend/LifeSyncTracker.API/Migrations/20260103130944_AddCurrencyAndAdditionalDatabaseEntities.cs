b1fb2039c9edf2869f89c3ed5a11df44effe7b16
Radovan Draskovic <73-2021@pmf.kg.ac.rs>
2026-01-03 14:17:01 +0100

    Add Currency field to Transactions and update related data

---
+++ backend/LifeSyncTracker.API/Migrations/20260103130944_AddCurrencyAndAdditionalDatabaseEntities.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LifeSyncTracker.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrencyAndAdditionalDatabaseEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Currency",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 3, 13, 9, 44, 258, DateTimeKind.Utc).AddTicks(5656));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 3, 13, 9, 44, 258, DateTimeKind.Utc).AddTicks(5667));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 3, 13, 9, 44, 258, DateTimeKind.Utc).AddTicks(5669));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 3, 13, 9, 44, 258, DateTimeKind.Utc).AddTicks(5671));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 3, 13, 9, 44, 258, DateTimeKind.Utc).AddTicks(5672));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 3, 13, 9, 44, 258, DateTimeKind.Utc).AddTicks(5673));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 3, 13, 9, 44, 258, DateTimeKind.Utc).AddTicks(5675));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 3, 13, 9, 44, 258, DateTimeKind.Utc).AddTicks(5676));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 3, 13, 9, 44, 258, DateTimeKind.Utc).AddTicks(5677));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 3, 13, 9, 44, 258, DateTimeKind.Utc).AddTicks(5678));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 3, 13, 9, 44, 258, DateTimeKind.Utc).AddTicks(5680));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 3, 13, 9, 44, 258, DateTimeKind.Utc).AddTicks(5681));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "Transactions");
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 2, 22, 24, 24, 747, DateTimeKind.Utc).AddTicks(4168));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 2, 22, 24, 24, 747, DateTimeKind.Utc).AddTicks(4175));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 2, 22, 24, 24, 747, DateTimeKind.Utc).AddTicks(4177));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 2, 22, 24, 24, 747, DateTimeKind.Utc).AddTicks(4179));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 2, 22, 24, 24, 747, DateTimeKind.Utc).AddTicks(4181));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 2, 22, 24, 24, 747, DateTimeKind.Utc).AddTicks(4183));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 2, 22, 24, 24, 747, DateTimeKind.Utc).AddTicks(4184));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 2, 22, 24, 24, 747, DateTimeKind.Utc).AddTicks(4186));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 2, 22, 24, 24, 747, DateTimeKind.Utc).AddTicks(4187));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 2, 22, 24, 24, 747, DateTimeKind.Utc).AddTicks(4189));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 2, 22, 24, 24, 747, DateTimeKind.Utc).AddTicks(4190));
+
+            migrationBuilder.UpdateData(
+                table: "TransactionCategories",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 2, 22, 24, 24, 747, DateTimeKind.Utc).AddTicks(4191));
+        }
+    }
+}
