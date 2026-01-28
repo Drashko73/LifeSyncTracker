5f1c3ad63396cb56ba4d8fc368ee28fbda897109
Radovan Draskovic <73-2021@pmf.kg.ac.rs>
2026-01-28 22:53:10 +0100

    Add health controller that is going to be used to check if backend service is up and running

---
+++ backend/LifeSyncTracker.API/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LifeSyncTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+
+        /// <summary>
+        /// Gets the health status of the backend API.
+        /// </summary>
+        /// <returns>Object containing the status and current UTC time.</returns>
+        [HttpGet("healthcheck")]
+        public IActionResult HealthCheck()
+        {
+            return Ok(new { status = "Healthy", timestampUtc = DateTime.UtcNow });
+        }
+
+    }
+}
